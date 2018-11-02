dbef7133d829e112e53a15b0c15c3a091aa41529
bhagyo <prachinmon1995@gmail.com>
2018-11-02 19:31:11 +0600

    test code of codeforces during contest

--- codeforces/extra.cpp
+++ codeforces/extra.cpp
@@ -1,63 +1,135 @@
 #include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<cstdio>
+#include<cstring>
+#include<string>
+#include<cmath>
+#include<iostream>
+#include<cctype>
+#include<map>
+#include<stack>
+#include<cstdlib>
+#include<queue>
+#include<vector>
+#include<algorithm>
+#include<bitset>
+#include<algorithm>
+#include<set>
+#include<limits.h>
+#include <sstream>
+
+using namespace std;
+typedef long long int lld;
+typedef unsigned long long int llu;
+
+#define sf scanf
+#define pf printf
+#define ff first
+#define ss second
+#define pii pair<int,int>
+#define PI acos(-1.0)
+#define sq(x) (x)*(x)
+#define nn printf("\n")
+#define mem(arr,val) memset(arr,val,sizeof(arr))
+#define TEST int test,zz;scanf("%d",&zz);getchar();for(test=1;test<=zz;test++)
+
+#define sci(x) scanf("%d",&x)
+#define sci2(x,y) scanf("%d %d",&x,&y)
+#define sci3(x,y,z) scanf("%d %d %d",&x,&y,&z)
+
+#define pfi(x) printf("%d\n",x)
+#define pfi2(x,y) printf("%d %d\n",x,y)
+#define pfi3(x,y,z) printf("%d %d %d\n",x,y,z)
+
+#define scs(str) scanf("%s",str)
+
+#define pfs(str) printf("%s\n",str)
+
+#define pb push_back
+
+#define chk1 printf("chek1\n")
+#define chk2 printf("chek2\n")
+#define chk3 printf("chek3\n")
+#define sz 10005
+#define sz1 23
+
+/******   start your code   *******/
+
+vector<string> vec;
+string str;
+struct node
+{
+    node* next[26 + 1];
+    int cnt[26+1];
+    node()
+    {
+        for (int i = 0; i < 26; i++)
+            next[i] = NULL,cnt[i]=0;
+    }
+} *root;
+void _insert(int len)
+{
+    node* curr=root;
+    for (int i=0; i<len; i++)
+    {
+        int id=str[i]-'a';
+        if(curr->next[id]==NULL)
+            curr->next[id] = new node();
+        curr->cnt[id]++;
+        curr = curr->next[id];
+    }
+}
+int _search(int len)
+{
+    node* curr = root;
+    int mx=-1;
+    for (int i = 0; i < len; i++)
+    {
+        int id = str[i] - 'a';
+        if(curr->cnt[id]==1)
+            return i;
+        curr=curr->next[id];
+    }
+    return len;
+}
 int main()
 {
-    int n;
-    int i;
-    int a,b,c;
-    int count;
-    int x,y=0;
-    int temp;
-    scanf("%d",&n);
-    for(i=0; i<n; i++)
+    root = new node();
+    while(getline(cin,str))
     {
-        int flag=0;
-        count=0;
-        y=0,x=0;
-        scanf("%d%d%d",&a,&b,&c);
-        if(c>a && c>b)
-        {
-            printf("-1\n");
-            continue;
-        }
-        if(a==c||b==c)
-        {
-            printf("1\n");
-            continue;
-        }
-        else
-        {
-            if(a<b)
-            {
-                temp=a;
-                a=b;
-                b=temp;
-            }
-            while(1)
-            {
-                x=b;
-                count++;
-                y=y+x;
-                count++;
-                if(a-y==c||y==c)
-                    break;
-                if(y>a)
-                {
-                    x=y-a;
-                    y=a;
-                    if(x==c)
-                        break;
-                    y=0;
-                    count++;
-                    y=x;
-                    count++;
-                    if(a-y==c)
-                        break;
-                }
-                x=0;
-            }
-            if(flag!=1)
-                printf("%d\n",count);
-        }
+        if(str.size()==0) break;
+        vec.pb(str);
+        _insert(str.size());
+    }
+    int i,lop,see,j;
+    lop=vec.size();
+    for(i=0; i<lop; i++)
+    {
+        str=vec[i];
+        see=_search(str.size());
+        cout<<str<<" ";
+        for(j=0; j<=see; j++)
+            pf("%c",str[j]);
+        nn;
     }
     return 0;
 }
+/*
+carbohydrate
+cart
+carburetor
+caramel
+caribou
+carbonic
+cartilage
+carbon
+carriage
+carton
+car
+carbonate
+
+Output
+*/
